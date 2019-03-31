263311c07baa694ecdbbef24cbd254b0b89161a0
Jhonatam Medina <jhonatam.forever@gmail.com>
2019-03-31 11:18:03 -0300

    Começando o capitulo 2

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -4,6 +4,7 @@ import { CartItem } from '../../models/cart-Item';
 import { ProdutoService } from '../../services/domain/produto.service';
 import { API_CONFIG } from '../../config/api.config';
 import { CartService } from '../../services/domain/cart.service';
+import { ProdutoDTO } from '../../models/produto.dto';
 
 
 
@@ -35,6 +36,22 @@ export class CartPage {
       }, error => {});
       
     }
+  }
+
+  removeItem(produto: ProdutoDTO){
+    this.items = this.cartService.removeProduto(produto).items;
+  }
+  increaseQuantity(produto: ProdutoDTO){
+    this.items = this.cartService.increaseQuantity(produto).items;
+  }
+  decreaseQuantity(produto: ProdutoDTO){
+    this.items = this.cartService.decraseQuantity(produto).items;
+  }
+  total() : number{
+    return this.cartService.total();
+  }
+  goOn(){
+    this.navCtrl.setRoot('CategoriasPage');
   }
 
 }
